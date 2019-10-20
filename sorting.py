8981a622d110559d0f3f472779eb58e8f2505b61
Sai Sunku <47285521+saisunku@users.noreply.github.com>
2019-10-20 11:34:38 -0400

    New test for draw heap

--- sorting.py
+++ sorting.py
@@ -173,7 +173,7 @@ class heap(object):         # Heap class
         return str_list
         
 
-A = heap([1, 3, 9, 10, 14, 8, 2, 4, 16, 7, 36, 7, 8, 99, 12, 5, 43, 1, 1])
+A = heap([1, 3, 9, 10, 14, 8, 2, 4, 16, 7, 36, 7, 8, 99, 12, 5, 43, 1, 1, 56, 5, 23, 67, 8, 9, 4, 65, 6, 22])
 
 # Check that the left and right functions work as expected
 assert(A.left(2)==5)
@@ -190,187 +190,182 @@ for j in range(A.heap_size+1):
     if r <= A.heap_size:
         assert A.heap[A.right(j)] <= A.heap[j]
 
-str_list = A.draw_heap(spacing=2)
+str_list = A.draw_heap()
 
 
 
-## Heapsort algorithm
-#def heapsort(array):
-#    
-#    # Build the heap
-#    H = heap(array)
-#    
-#    # Take out the first element and reduce the size of the heap
-#    for j in range(len(array)):
-#        # Swap the largest element in the heap with the last element in the heap
-#        tmp = H.heap[0]
-#        H.heap[0] = H.heap[H.heap_size]   
-#        H.heap[H.heap_size] = tmp
-#        
-#        # Decrement heap size
-#        H.heap_size -= 1
-#        
-#        # Max-Heapify
-#        H.max_heapify(0)
-#        
-#    return H.heap
-#
-#array = [5, 2, 4, 6, 1, 3]
-##print(heapsort(array))
-#assert heapsort(array) == [1, 2, 3, 4, 5, 6]
-#array = [31, 41, 59, 26, 41, 58]
-##print(heapsort(array))
-#assert heapsort(array) == [26, 31, 41, 41, 58, 59]
-#
-#
-## Quicksort - Chapter 7 CLRS
-#
-## First define the partioning function
-#def partition(A, p=0, r=None):
-#    # Partition the array A[p:r] for use with Quicksort    
-#    if r == None:
-#        r = len(A)-1
-#
-#    i = p - 1    
-#    for j_iter in range(r-p):
-#        j = j_iter + p
-##        print(j)
-##        print(A)
-#        
-#        if A[j] < A[r]:
-#            i = i + 1
-#            tmp = A[i]
-#            A[i] = A[j]
-#            A[j] = tmp
-#    
-#    tmp = A[i+1]
-#    A[i+1] = A[r]
-#    A[r] = tmp
-#    
-#    return i + 1
-#
-#array = [5, 2, 4, 6, 1, 3]
-#partition(array)
-#assert array == [2, 1, 3, 6, 5, 4]
-#
-#array = [2, 8, 7, 1, 3, 5, 6, 4]
-#partition(array)
-#assert array == [2, 1, 3, 4, 7, 5, 6, 8]
-#
-## Quicksort
-#def quicksort(A, p=0, r=None):
-#    if r == None:
-#        r = len(A)-1
-#    
-##    print(p, r)
-##    print(A)
-#    
-#    if p < r:
-#        q = partition(A, p, r)
-#        quicksort(A, p, q-1)
-#        quicksort(A, q+1, r)
-#    
-#    return A
-#
-#array = [5, 2, 4, 6, 1, 3]
-##print(quicksort(array))
-#assert quicksort(array) == [1, 2, 3, 4, 5, 6]
-#
-#array = [31, 41, 59, 26, 41, 58]
-##print(quicksort(array))
-#assert quicksort(array) == [26, 31, 41, 41, 58, 59]
-#
-#
-## Randomized partition
-#def partition_random(A, p=0, r=None):
-#    if r == None:
-#        r = len(A)-1
-#        
-#    rand_idx = random.randint(p, r)
-#    tmp = A[rand_idx]
-#    A[rand_idx] = A[r]
-#    A[r] = tmp
-#    
-#    return partition(A, p, r)
-#
-#array = [5, 2, 4, 6, 1, 3]
-#partition_random(array)
-#print(array)
-#
-#def quicksort_random(A, p=0, r=None):
-#    if r == None:
-#        r = len(A)-1
-#        
-#    if p < r:
-#        q = partition_random(A, p, r)
-#        quicksort_random(A, p, q-1)
-#        quicksort_random(A, q+1, r)
-#        
-#    return A
-#
-#array = [5, 2, 4, 6, 1, 3]
-##print(quicksort(array))
-#assert quicksort_random(array) == [1, 2, 3, 4, 5, 6]
-#
-#array = [31, 41, 59, 26, 41, 58]
-##print(quicksort(array))
-#assert quicksort_random(array) == [26, 31, 41, 41, 58, 59]
-#
-#
-## Given array A of size n which contains integers from p to p+n, sort A in place
-#def consecutive_numbers_sort_in_place(A, p=0):
-#    j = 0
-#    for j in range(len(A)):
-#        while A[j] != p+j:
-#            pos = A[j] - p
-#            
-#            tmp = A[pos]
-#            A[pos] = A[j]
-#            A[j] = tmp
-#    return A
-#            
-#A = [1, 2, 4, 0, 5, 7, 6, 8, 10, 9, 3]
-#assert consecutive_numbers_sort_in_place(A) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-#
-#
-## Counting sort - Pg 195
-#def counting_sort(A, max_elem=None):
-#    if max_elem == None:
-#        max_elem = max(A)
-#    
-#    # Initialize the output and count arrays
-#    out = [0]*len(A)
-#    count = [0]*(max_elem+1) # Including zeros
-#    
-#    # Populate the count array
-#    for val in A:
-#        count[val] += 1
-##    print(count)
-#        
-#    # Keep a running sum
-#    for idx in range(len(count)-1):
-#        idx = idx + 1
-#        count[idx] = count[idx] + count[idx-1]
-##    print(count)
-#        
-#    # Now sort
-#    for idx in range(len(A)-1,-1,-1):
-#        out[count[A[idx]]-1] = A[idx]
-#        count[A[idx]] -= 1
-#        
-#    return out
-#    
-#array = [0, 4, 5, 1, 5, 2, 3, 4, 2, 0, 0, 3, 1, 2, 4]
-#assert counting_sort(array) == [0, 0, 0, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5]
-#
-#array = [0, 1, 2, 5, 6, 9, 7, 8, 4, 4, 6, 3, 8, 5, 2, 7, 9, 0, 1, 5, 7, 4, 2, 6, 9]
-#assert counting_sort(array) == [0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9]
-#
-#array = [1, 1, 1, 1, 1]
-#assert counting_sort(array) == [1, 1, 1, 1, 1]
-#
-#
-#        
-#    
-#
-#    
+# Heapsort algorithm
+def heapsort(array):
+    
+    # Build the heap
+    H = heap(array)
+    
+    # Take out the first element and reduce the size of the heap
+    for j in range(len(array)):
+        # Swap the largest element in the heap with the last element in the heap
+        tmp = H.heap[0]
+        H.heap[0] = H.heap[H.heap_size]   
+        H.heap[H.heap_size] = tmp
+        
+        # Decrement heap size
+        H.heap_size -= 1
+        
+        # Max-Heapify
+        H.max_heapify(0)
+        
+    return H.heap
+
+array = [5, 2, 4, 6, 1, 3]
+#print(heapsort(array))
+assert heapsort(array) == [1, 2, 3, 4, 5, 6]
+array = [31, 41, 59, 26, 41, 58]
+#print(heapsort(array))
+assert heapsort(array) == [26, 31, 41, 41, 58, 59]
+
+
+# Quicksort - Chapter 7 CLRS
+
+# First define the partioning function
+def partition(A, p=0, r=None):
+    # Partition the array A[p:r] for use with Quicksort    
+    if r == None:
+        r = len(A)-1
+
+    i = p - 1    
+    for j_iter in range(r-p):
+        j = j_iter + p
+#        print(j)
+#        print(A)
+        
+        if A[j] < A[r]:
+            i = i + 1
+            tmp = A[i]
+            A[i] = A[j]
+            A[j] = tmp
+    
+    tmp = A[i+1]
+    A[i+1] = A[r]
+    A[r] = tmp
+    
+    return i + 1
+
+array = [5, 2, 4, 6, 1, 3]
+partition(array)
+assert array == [2, 1, 3, 6, 5, 4]
+
+array = [2, 8, 7, 1, 3, 5, 6, 4]
+partition(array)
+assert array == [2, 1, 3, 4, 7, 5, 6, 8]
+
+# Quicksort
+def quicksort(A, p=0, r=None):
+    if r == None:
+        r = len(A)-1
+    
+#    print(p, r)
+#    print(A)
+    
+    if p < r:
+        q = partition(A, p, r)
+        quicksort(A, p, q-1)
+        quicksort(A, q+1, r)
+    
+    return A
+
+array = [5, 2, 4, 6, 1, 3]
+#print(quicksort(array))
+assert quicksort(array) == [1, 2, 3, 4, 5, 6]
+
+array = [31, 41, 59, 26, 41, 58]
+#print(quicksort(array))
+assert quicksort(array) == [26, 31, 41, 41, 58, 59]
+
+
+# Randomized partition
+def partition_random(A, p=0, r=None):
+    if r == None:
+        r = len(A)-1
+        
+    rand_idx = random.randint(p, r)
+    tmp = A[rand_idx]
+    A[rand_idx] = A[r]
+    A[r] = tmp
+    
+    return partition(A, p, r)
+
+array = [5, 2, 4, 6, 1, 3]
+partition_random(array)
+print(array)
+
+def quicksort_random(A, p=0, r=None):
+    if r == None:
+        r = len(A)-1
+        
+    if p < r:
+        q = partition_random(A, p, r)
+        quicksort_random(A, p, q-1)
+        quicksort_random(A, q+1, r)
+        
+    return A
+
+array = [5, 2, 4, 6, 1, 3]
+#print(quicksort(array))
+assert quicksort_random(array) == [1, 2, 3, 4, 5, 6]
+
+array = [31, 41, 59, 26, 41, 58]
+#print(quicksort(array))
+assert quicksort_random(array) == [26, 31, 41, 41, 58, 59]
+
+
+# Given array A of size n which contains integers from p to p+n, sort A in place
+def consecutive_numbers_sort_in_place(A, p=0):
+    j = 0
+    for j in range(len(A)):
+        while A[j] != p+j:
+            pos = A[j] - p
+            
+            tmp = A[pos]
+            A[pos] = A[j]
+            A[j] = tmp
+    return A
+            
+A = [1, 2, 4, 0, 5, 7, 6, 8, 10, 9, 3]
+assert consecutive_numbers_sort_in_place(A) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+# Counting sort - Pg 195
+def counting_sort(A, max_elem=None):
+    if max_elem == None:
+        max_elem = max(A)
+    
+    # Initialize the output and count arrays
+    out = [0]*len(A)
+    count = [0]*(max_elem+1) # Including zeros
+    
+    # Populate the count array
+    for val in A:
+        count[val] += 1
+#    print(count)
+        
+    # Keep a running sum
+    for idx in range(len(count)-1):
+        idx = idx + 1
+        count[idx] = count[idx] + count[idx-1]
+#    print(count)
+        
+    # Now sort
+    for idx in range(len(A)-1,-1,-1):
+        out[count[A[idx]]-1] = A[idx]
+        count[A[idx]] -= 1
+        
+    return out
+    
+array = [0, 4, 5, 1, 5, 2, 3, 4, 2, 0, 0, 3, 1, 2, 4]
+assert counting_sort(array) == [0, 0, 0, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5]
+
+array = [0, 1, 2, 5, 6, 9, 7, 8, 4, 4, 6, 3, 8, 5, 2, 7, 9, 0, 1, 5, 7, 4, 2, 6, 9]
+assert counting_sort(array) == [0, 0, 1, 1, 2, 2, 2, 3, 4, 4, 4, 5, 5, 5, 6, 6, 6, 7, 7, 7, 8, 8, 9, 9, 9]
+
+array = [1, 1, 1, 1, 1]
+assert counting_sort(array) == [1, 1, 1, 1, 1]
+
